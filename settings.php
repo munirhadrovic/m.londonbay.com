350a471abf446bbb8c50fe1957ab6ddc25217793
Emir <emiratesbp@gmail.com>
2015-04-23 11:10:13 +0200

    first commit

---
+++ settings.php
@@ -0,0 +1,41 @@
+<?php
+	$adresa[0]="2210 Vanderbilt Beach Road, Suite 1300"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[0]="Naples, FL 34109";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[0]="London Bay Corporate Office";
+	$telephone[0]=8669201754;// Broj koji se poziva na dugme CALL US
+
+	$adresa[1]="15836 Savona Way"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[1]="Naples, FL 34110";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[1]="MEDITERRA";
+	$telephone[1]=2392987600;// Broj koji se poziva na dugme CALL US
+
+	$adresa[2]="1482 Anhinga Point"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[2]="Naples, FL 34105";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[2]="MIRADA AT THE ESTUARY AT GREY OAKS";
+	$telephone[2]=2392807367;// Broj koji se poziva na dugme CALL US
+
+	$adresa[3]="18149 Via Portofino Way"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[3]="Miromar Lakes, FL 33913";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[3]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[3]="PORTOFINO AT MIROMAR LAKES";
+	$telephone[3]=2398254892;// Broj koji se poziva na dugme CALL US
+
+	$adresa[4]="3001 Founders Club Drive"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[4]="Sarasota, FL 34240";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[4]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[4]="THE FOUNDERS CLUB";
+	$telephone[4]=9414043107;// Broj koji se poziva na dugme CALL US
+
+	$adresa[5]="7979 Matera Court"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[5]="Lakewood Ranch, FL 34202";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[5]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[5]="THE LAKE CLUB AT LAKEWOOD RANCH";
+	$telephone[5]=9417733004;// Broj koji se poziva na dugme CALL US
+
+	$companynumber=6;
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="turn"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
